f6292ef6ebbe0ffb0b13d1ee44b4d27c96e39a60
st3pbyst3p <cornel.scoarta@gmail.com>
2023-03-17 12:13:52 +0100

    fix: build

--- dist/ui-bootstrap-2.5.4.js
+++ dist/ui-bootstrap-2.5.4.js
@@ -2,7 +2,7 @@
  * angular-ui-bootstrap
  * http://angular-ui.github.io/bootstrap/
 
- * Version: 2.5.4 - 2018-12-14
+ * Version: 2.5.4 - 2023-03-17
  * License: MIT
  */angular.module("ui.bootstrap", ["ui.bootstrap.collapse","ui.bootstrap.tabindex","ui.bootstrap.accordion","ui.bootstrap.alert","ui.bootstrap.buttons","ui.bootstrap.carousel","ui.bootstrap.dateparser","ui.bootstrap.isClass","ui.bootstrap.datepicker","ui.bootstrap.position","ui.bootstrap.datepickerPopup","ui.bootstrap.debounce","ui.bootstrap.multiMap","ui.bootstrap.dropdown","ui.bootstrap.stackedMap","ui.bootstrap.modal","ui.bootstrap.paging","ui.bootstrap.pager","ui.bootstrap.pagination","ui.bootstrap.tooltip","ui.bootstrap.popover","ui.bootstrap.progressbar","ui.bootstrap.rating","ui.bootstrap.tabs","ui.bootstrap.timepicker","ui.bootstrap.typeahead"]);
 angular.module('ui.bootstrap.collapse', [])
@@ -1325,10 +1325,14 @@ angular.module('ui.bootstrap.dateparser', [])
     var convertedDate = date.toLocaleDateString('ru-RU', options);
 
     // getting the offset
-    var splitted = convertedDate.split(',');
-    if(splitted.length > 2) { splitted = splitted[2].split(' '); }
-    if(splitted.length > 2) { splitted = splitted[2].slice(3);   }
-    var offset = splitted;
+    // var splitted = convertedDate.split(',');
+    // if(splitted.length > 2) { splitted = splitted[2].split(' '); }
+    // if(splitted.length > 2) { splitted = splitted[2].slice(3);   }
+    // var offset = splitted;
+
+    // getting the offset
+    var splitted = convertedDate.split('GMT');
+    var offset = splitted[1];
 
     // transforming the offset into a suitable format
     var serverOffset = -60*parseInt(offset);
--- dist/ui-bootstrap-tpls-2.5.4.js
+++ dist/ui-bootstrap-tpls-2.5.4.js
@@ -2,7 +2,7 @@
  * angular-ui-bootstrap
  * http://angular-ui.github.io/bootstrap/
 
- * Version: 2.5.4 - 2018-12-14
+ * Version: 2.5.4 - 2023-03-17
  * License: MIT
  */angular.module("ui.bootstrap", ["ui.bootstrap.tpls", "ui.bootstrap.collapse","ui.bootstrap.tabindex","ui.bootstrap.accordion","ui.bootstrap.alert","ui.bootstrap.buttons","ui.bootstrap.carousel","ui.bootstrap.dateparser","ui.bootstrap.isClass","ui.bootstrap.datepicker","ui.bootstrap.position","ui.bootstrap.datepickerPopup","ui.bootstrap.debounce","ui.bootstrap.multiMap","ui.bootstrap.dropdown","ui.bootstrap.stackedMap","ui.bootstrap.modal","ui.bootstrap.paging","ui.bootstrap.pager","ui.bootstrap.pagination","ui.bootstrap.tooltip","ui.bootstrap.popover","ui.bootstrap.progressbar","ui.bootstrap.rating","ui.bootstrap.tabs","ui.bootstrap.timepicker","ui.bootstrap.typeahead"]);
 angular.module("ui.bootstrap.tpls", ["uib/template/accordion/accordion-group.html","uib/template/accordion/accordion.html","uib/template/alert/alert.html","uib/template/carousel/carousel.html","uib/template/carousel/slide.html","uib/template/datepicker/datepicker.html","uib/template/datepicker/day.html","uib/template/datepicker/month.html","uib/template/datepicker/year.html","uib/template/datepickerPopup/popup.html","uib/template/modal/window.html","uib/template/pager/pager.html","uib/template/pagination/pagination.html","uib/template/tooltip/tooltip-html-popup.html","uib/template/tooltip/tooltip-popup.html","uib/template/tooltip/tooltip-template-popup.html","uib/template/popover/popover-html.html","uib/template/popover/popover-template.html","uib/template/popover/popover.html","uib/template/progressbar/bar.html","uib/template/progressbar/progress.html","uib/template/progressbar/progressbar.html","uib/template/rating/rating.html","uib/template/tabs/tab.html","uib/template/tabs/tabset.html","uib/template/timepicker/timepicker.html","uib/template/typeahead/typeahead-match.html","uib/template/typeahead/typeahead-popup.html"]);
@@ -1326,10 +1326,14 @@ angular.module('ui.bootstrap.dateparser', [])
     var convertedDate = date.toLocaleDateString('ru-RU', options);
 
     // getting the offset
-    var splitted = convertedDate.split(',');
-    if(splitted.length > 2) { splitted = splitted[2].split(' '); }
-    if(splitted.length > 2) { splitted = splitted[2].slice(3);   }
-    var offset = splitted;
+    // var splitted = convertedDate.split(',');
+    // if(splitted.length > 2) { splitted = splitted[2].split(' '); }
+    // if(splitted.length > 2) { splitted = splitted[2].slice(3);   }
+    // var offset = splitted;
+
+    // getting the offset
+    var splitted = convertedDate.split('GMT');
+    var offset = splitted[1];
 
     // transforming the offset into a suitable format
     var serverOffset = -60*parseInt(offset);
