4d7adceb69f4c7ec9cf1edcb602051d05a5829a0
st3pbyst3p <cornel.scoarta@gmail.com>
2026-04-24 20:16:39 +0300

    fix: added translate directive

--- dist/ui-bootstrap-tpls-2.5.4.js
+++ dist/ui-bootstrap-tpls-2.5.4.js
@@ -7672,10 +7672,10 @@ angular.module("uib/template/datepickerPopup/popup.html", []).run(["$templateCac
     "  <li ng-transclude></li>\n" +
     "  <li ng-if=\"showButtonBar\" class=\"uib-button-bar\">\n" +
     "    <span class=\"btn-group pull-left\">\n" +
-    "      <button type=\"button\" class=\"btn btn-sm btn-info uib-datepicker-current\" ng-click=\"select('today', $event)\" ng-disabled=\"isDisabled('today')\">{{ getText('current') }}</button>\n" +
-    "      <button type=\"button\" class=\"btn btn-sm btn-danger uib-clear\" ng-click=\"select(null, $event)\">{{ getText('clear') }}</button>\n" +
+    "      <button type=\"button\" class=\"btn btn-sm btn-info uib-datepicker-current\" ng-click=\"select('today', $event)\" ng-disabled=\"isDisabled('today')\"><span translate>{{ getText('current') }}</span></button>\n" +
+    "      <button type=\"button\" class=\"btn btn-sm btn-danger uib-clear\" ng-click=\"select(null, $event)\"><span translate>{{ getText('clear') }}</span></button>\n" +
     "    </span>\n" +
-    "    <button type=\"button\" class=\"btn btn-sm btn-success pull-right uib-close\" ng-click=\"close($event)\">{{ getText('close') }}</button>\n" +
+    "    <button type=\"button\" class=\"btn btn-sm btn-success pull-right uib-close\" ng-click=\"close($event)\"><span translate>{{ getText('close') }}</span></button>\n" +
     "  </li>\n" +
     "</ul>\n" +
     "");
